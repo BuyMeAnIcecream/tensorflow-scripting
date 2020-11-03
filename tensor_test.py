dea2ea92de704f666f6d36fd80dddd34a841a61c
BuyMeAnIceCream <diffrectingbull@gmail.com>
2020-11-03 12:27:00 -1000

    added test data set - added confusion matrix but plot won't plot. python fonts error

--- tensor_test.py
+++ tensor_test.py
@@ -1,4 +1,5 @@
 #PREPARE DATA
+
 import numpy as np
 from random import randint
 from sklearn.utils import shuffle
@@ -41,7 +42,7 @@ scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1,1))
 #for i in scaled_train_samples:
 #    print(i)
 
-input("press close to exit") 
+
 
 #CONSTRUCT SEQUENTIAL NN
 
@@ -82,3 +83,95 @@ for i in rounded_prediction:
     print(i)
 
 
+#PREPROCESS TEST DATA
+test_labels = []
+test_samples = []
+
+for i in range(10):
+    #the ~5% of younger individuals who did exp side eff
+    random_younger = randint(13,64)
+    test_samples.append(random_younger)
+    test_labels.append(1)
+
+    #the ~5% of older individuals who did not exp side eff
+    random_older = randint(65,100)
+    test_samples.append(random_older)
+    test_labels.append(0)
+
+for i in range(200):
+    #the ~95% of younger individuals who did not exp side eff
+    random_younger = randint(13,64)
+    test_samples.append(random_younger)
+    test_labels.append(0)
+
+    #the ~95% of older individuals who did exp side eff
+    random_older = randint(65,100)
+    test_samples.append(random_older)
+    test_labels.append(1)
+
+test_samples = np.array(test_samples)
+test_labels = np.array(test_labels)
+test_labels, test_samples = shuffle(test_labels, test_samples);
+
+scaled_test_samples = scaler.fit_transform(test_samples.reshape(-1,1))
+
+predictions = model.predict(x = scaled_test_samples, batch_size = 10, verbose = 0)
+for i in predictions:
+    print(i)
+    
+rounded_prediction = np.argmax(predictions, axis = -1)
+
+for i in rounded_prediction:
+    print(i)
+
+input("press close to exit") 
+
+#CONFUSION MATRIX
+#%matplotlib inline
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib
+matplotlib.use("tkagg")
+import matplotlib.pyplot as plt
+
+cm = confusion_matrix(y_true = test_labels, y_pred = rounded_prediction)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize = False,
+                          title = 'Cunfusion matrix',
+                          cmap = plt.cm.Blues):
+    """
+    Prints and plots the confusion matrix.
+    Normalization can be applied by setting 'normalize  = True'.
+    """
+    
+    plt.imshow(cm,interpolation = 'nearest', cmap = cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arrange(len(classes))
+    plt.xticks(tick_marks, classes, rotation = 45)
+    plt.yticks(tick_marks, classes)
+    
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis = 1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print("Confusion matrix, without norm")
+        
+    print(cm)
+    
+    thresh = cm.max / 2.
+    for i,j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i,j],
+            horizontalalignment = "center",
+            color = "white" if cm[i,j] > thresh else "black")
+            
+            
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predict label')
+    
+
+    
+    cm_plot_labels = ['no_side_effects', 'had_side_effects']
+    plot_confusion_matrix(cm = cm, classes = cm_plot_labels, title = 'Confusion Matrix')
